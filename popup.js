3cfca25b9659182ce0de092c8c3a96937780c6ea
orgwirelist123 <orgwirelist123@gmail.com>
2020-02-13 22:25:48 -0500

    yes

--- popup.js
+++ popup.js
@@ -18,5 +18,68 @@ function sendMessage (event){
     document.getElementById("messages")    */
 }
 
+var wsUri = "wss://echo.websocket.org/";
+var output;
+
+function init()
+{
+  output = document.getElementById("chatbox");
+  testWebSocket();
+}
+
+function testWebSocket()
+{
+  websocket = new WebSocket(wsUri);
+  websocket.onopen = function(evt) { onOpen(evt) };
+  websocket.onclose = function(evt) { onClose(evt) };
+  websocket.onmessage = function(evt) { onMessage(evt) };
+  websocket.onerror = function(evt) { onError(evt) };
+}
+
+function onOpen(evt)
+{
+  writeToScreen("CONNECTED");
+  doSend("WebSocket rocks");
+}
+
+function onClose(evt)
+{
+  writeToScreen("DISCONNECTED");
+}
+
+function onMessage(evt)
+{
+  writeToScreen('<span style="color: blue;">RESPONSE: ' + evt.data+'</span>');
+  websocket.close();
+}
+
+function onError(evt)
+{
+  writeToScreen('<span style="color: red;">ERROR:</span> ' + evt.data);
+}
+
+function doSend(message)
+{
+  writeToScreen("SENT: " + message);
+  websocket.send(message);
+}
+
+function sendMessage()
+{
+    message = document.getElementById("usermsg").innerText;
+    console.log("Message:" + message)
+    doSend(message);
+}
+
+function writeToScreen(message)
+{
+  var pre = document.createElement("p");
+  pre.style.wordWrap = "break-word";
+  pre.innerHTML = message;
+  output.appendChild(pre);
+}
+
+window.addEventListener("load", init, false);
+
 document.getElementById('username').addEventListener("input", username)
-document.getElementById('submitmsg').addEventListener('click', sendMessage)
+//document.getElementById('submitmsg').addEventListener('click', sendMessage)
